780516251a0b2b7233ed7eb59472567050a3ec6c
bafeigum <bafeigum@gmail.com>
2018-11-24 17:01:14 -0800

    Update main.c

--- main.c
+++ main.c
@@ -72,7 +72,7 @@ void print_hex_16(int val) {
 
 int main()
 {
-	const uint8_t greeting[] = "\n\rRISC-V SoftCPU Contest 2018 bootloader for bit_riscv\n\rCredit to Antti Lukats\n\rUsing Cortex-M3 assisted boot concept and UHSA\n\r";
+	const uint8_t greeting[] = "\n\rRISC-V SoftCPU Contest 2018 bootloader for bit_riscv\n\rCredit to Antti Lukats\n\rUsing Cortex-M3 assisted boot concept\n\rRev: 0.0.2\n\r";
 
 
 	const int* AddressFabricRAM = 0x50000000;
@@ -165,10 +165,10 @@ int main()
     {
     	w = *nvm_app; nvm_app++;
 
-    	*sram_app = w & 0xFFFF;    	sram_app++;
+    	*sram_app = w;    	sram_app++;
 		//print_hex_16(w); alt_putchar(10);alt_putchar(13);
 
-    	*sram_app = w >> 16;    	sram_app++;
+    	//*sram_app = w >> 16;    	sram_app++;
 		//print_hex_16(w >> 16); alt_putchar(10);alt_putchar(13);
 		//print_hex_16(i); alt_putchar(10);alt_putchar(13);
     }
@@ -179,7 +179,7 @@ int main()
 
 
 
-    for (i=0; i < 10; i++)
+    for (i=0; i < 25; i++)
     {
 		delay();
 		alt_putchar('.');
@@ -209,155 +209,7 @@ int main()
 
     /* Event loop never exits. */
     while (1) {
-  	  	  if (done) {
-  	  		c0 = 0;
-  	  		done = 0;
-  	  	  } else {
-  	  		c0 = c;
-  	  	  }
-
-  		  c = alt_getchar();
-  		  //x++;
-  		  //*lsram = x;
-
-
-  		  // pre-process HEX nibble in all cases for the case we need it
-  		  is_nibble = 1;
-  		  switch (c) {
-  		  case '0':
-  		  	  b = 0;
-  			  break;
-  		  case '1':
-  		  	  b = 1;
-  			  break;
-  		  case '2':
-  		  	  b = 2;
-  			  break;
-  		  case '3':
-  		  	  b = 3;
-  			  break;
-  		  case '4':
-  		  	  b = 4;
-  			  break;
-  		  case '5':
-  		  	  b = 5;
-  			  break;
-  		  case '6':
-  		  	  b = 6;
-  			  break;
-  		  case '7':
-  		  	  b = 7;
-  			  break;
-  		  case '8':
-  		  	  b = 8;
-  			  break;
-  		  case '9':
-  		  	  b = 9;
-  			  break;
-  		  case 'a':
-  		  	  b = 10;
-  			  break;
-  		  case 'b':
-  		  	  b = 11;
-  			  break;
-  		  case 'c':
-  		  	  b = 12;
-  			  break;
-  		  case 'd':
-  		  	  b = 13;
-  			  break;
-  		  case 'e':
-  		  	  b = 14;
-  			  break;
-  		  case 'f':
-  		  	  b = 15;
-  			  break;
-  		  default:
-  			  b = 0;
-  			  is_nibble = 0;
-  		  }
-  		  // Escape processing first
-  		  if (in_escape) {
-  			  if (c==UHSA_ESCAPE_END) {
-  				  in_escape = 0;
-  				  done = 1;
-  				  // processing done
-  			  } else {
-  				  if (c==UHSA_ESCAPE) {
-  					  in_escape = 0;
-  					  //done = 1;
-  				  } else {
-  					  // regular ESCAPE block processing
-  					  if ((is_nibble) && (c0==UHSA_ESCAPE)) {
-  						  channel = b;
-  						  //alt_printf('{%u}',b);
-  						  //alt_putchar('{');
-    						  //alt_putchar(channel + '0');
-    						  //alt_putchar('}');
-
-  					  } else {
-  						  //alt_putchar('(');
-  						  //alt_putchar(c);
-  						  //alt_putchar(')');
-  					  }
-
-  				  }
-  			  }
-  		  }
-
-  		  if (!in_escape) {
-  			  // Process normal stream after escape processing block
-  			  if ((c==UHSA_ESCAPE) && (c0!=UHSA_ESCAPE)) {
-  				  in_escape = 1;
-  				  // processing done
-  			  } else {
-  				  // Process normally here we are not inside escape sequence
-  				  if (!done) {
-  					  switch (channel) {
-  					  case 0:
-  						  if (is_nibble) {
-  							  //spi_xfer4(b);
-//todo  							  *lsram = (b << 24) | b;
-//todo							  print_hex_char(temp);
-
-  						  } else {
-  							  switch (c) {
-  							  case '.':
-  							  	  //temp = spi_xfer4(0);
-  							  	  print_hex_char(temp);
-  								  break;
-  							  case '<':
-  								  // Activate Chip Select
-  								  break;
-  							  case '>':
-  								  // De-activate Chip Select
-  								  break;
-  							  case 'r':
-  								  // assert Reset low
-  								  MSS_GPIO_set_outputs(0);
-  								  break;
-  							  case 'R':
-  								  // Let RISC-V run free
-  								  MSS_GPIO_set_outputs(1);
-  								  break;
-  							  }
-  						  }
-  						  break;
-
-  					  default:
-  						  // undefined/unused channel
-  						  alt_putchar('?');
-  						  alt_putchar(c);
-  						  					  }
-  					  if (c==UHSA_ESCAPE) {
-  						  done = 1;
-  					  }
-  				  }
-  			  }
-  		  }
-
-// Echo all..
-   	  	  alt_putchar(c);
+    	__WFI;
     };
 
 }
